9a47433d0a84084daf889921295efbf212233023
Pulvex24 <49303365+Pulvex24@users.noreply.github.com>
2024-11-05 12:40:32 -0500

    Add files via upload

---
+++ mad.py
@@ -0,0 +1,16 @@
+
+
+# String concatenation
+#youtuber = "Paioner"
+# Formas de agregar los datos de una variable en una cadena (string). Por lo general usaba la primera forma de concatenacion pero ahora uso la Tercera...
+#primera forma print("Suscribe to " + youtuber)
+#Segunda Forma print("Suscribe to {}".format(youtuber))
+#Tercera Forma print(f"Suscribe to {youtuber}")
+
+adj = input("Adjetive is: ")
+verb1 = input("Verbo one: ")
+verb2 = input("Verbo two: ")
+famous_p = input("Persona Famosa: ")
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because, i love to {verb1}. Stay hydrate and {verb2} like you are {famous_p}!"
+
+print(madlib)
